ebf93aeb1c958bb52c08f5fd9263cf064e6990b8
SIYU REN <s.ren1.23@abdn.ac.uk>
2024-03-14 14:46:48 +0000

    test

---
+++ bears/tests.py
@@ -0,0 +1,17 @@
+from django.test import Client, TestCase
+
+# Create your tests here.
+"""
+class TemperatureTests(TestCase):
+
+    def test_temp_text(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Enter the program!")
+"""
+
+
+    
+
+    
